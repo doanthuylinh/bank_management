c424974707f5f46abaa766d6ea92e4b4909e7db6
nguyendangthientam <nguyendangthientam1997@gmail.com>
2021-04-10 21:58:02 +0700

    Add ngrok-update

--- src/main/java/com/example/demo/BankManagementSpringBootsLinhDtApplication.java
+++ src/main/java/com/example/demo/BankManagementSpringBootsLinhDtApplication.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
 
 @SpringBootApplication
-public class BankManagementSpringBootsLinhDtApplication {
+public class BankManagementSpringBootsLinhDtApplication extends org.springframework.boot.web.servlet.support.SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BankManagementSpringBootsLinhDtApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BankManagementSpringBootsLinhDtApplication.class);
     }
 
 }
